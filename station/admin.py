0d3dfa740eda376e1c0fd8a97332b7f3ff52de69
Anastasiia Pohonets <29karasik@gmail.com>
2025-12-30 14:45:49 +0200

    Update admin models

--- station/admin.py
+++ station/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from station.models import (Route,
+                            TrainType,
+                            Station,
+                            Train,
+                            Crew,
+                            Journey,
+                            Order,
+                            Ticket)
+
+
+class TicketInLine(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInLine,)
+
+
+admin.site.register(Route)
+admin.site.register(TrainType)
+admin.site.register(Station)
+admin.register(Train)
+admin.site.register(Crew)
+admin.site.register(Journey)
+admin.site.register(Ticket)
