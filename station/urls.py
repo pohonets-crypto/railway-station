810da25cd892d55bcdb2ced0307d1e0f575d3620
Anastasiia Pohonets <29karasik@gmail.com>
2025-12-30 14:48:18 +0200

    Update station/urls

--- station/urls.py
+++ station/urls.py
@@ -1,8 +1,23 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from station.views import (TrainTypeViewSet,
+                           StationViewSet,
+                           RouteViewSet,
+                           TrainViewSet,
+                           CrewViewSet,
+                           JourneyViewSet,
+                           OrderViewSet)
 
 router = routers.DefaultRouter()
+router.register("train_types", TrainTypeViewSet)
+router.register("stations", StationViewSet)
+router.register("routes", RouteViewSet)
+router.register("trains", TrainViewSet)
+router.register("crews", CrewViewSet)
+router.register("journeys", JourneyViewSet)
+router.register("orders", OrderViewSet, basename="order")
+urlpatterns = []
 urlpatterns = [path("", include(router.urls))]
 
 app_name = "station"
